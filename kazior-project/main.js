117df064685cc03085b321ccfd515b5a3c2c6fc3
Le Marguesse <amargesono@gmail.com>
2022-04-22 16:46:33 +0600

    Shah & kazior deleted

--- kazior-project/main.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-    let selecter = document.getElementById("lang-selecter");
-    let selecterText = document.getElementsByClassName("selecter-text")[0];
-
-    selecter.addEventListener("change", function () {
-        selecterText.innerHTML = selecter.value;
-    })
-}
